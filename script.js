fa16037ca362288d8fa80571e03e46964b97155d
oberon4589 <oberon4589@gmail.com>
2024-04-01 09:08:39 -0300

    Adiciona lógica de jogo da velha ao script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('Hello World')
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttonsContainer button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+// Contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+// Adicionando o evento de click aos boxes
+for (let i = 0; i < boxes.length; i++) {
+  //clica na caixa
+  boxes[i].addEventListener("click", function () {
+    let el = checkEl(player1, player2);
+
+    //Verifica se já tem X ou O
+    if (this.childNodes.length == 0) {
+      let cloneEl = el.cloneNode(true);
+
+      this.appendChild(cloneEl);
+
+      //Computar a jogada
+
+      if (player1 == player2) {
+        player1++;
+      } else {
+        player2++;
+      }
+    }
+  });
+}
+
+// Função para checar quem vai jogar
+function checkEl(player1, player2) {
+  if (player1 == player2) {
+    el = x;
+  } else {
+    el = o;
+  }
+  return el;
+}
